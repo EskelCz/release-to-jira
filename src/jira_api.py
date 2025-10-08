6f1f94af65e91188da5f1cf08673231ebb2c57ac
David Spohr <spohr.david@gmail.com>
2025-10-08 15:50:22 +0200

    Fix get_or_create_release to handle release marking
    Update the release after creation, as the POST API doesn't allow creating it with "released" status.

--- src/jira_api.py
+++ src/jira_api.py
@@ -32,13 +32,19 @@ def get_project_id():
 
 
 def get_or_create_release(release_name):
-    result = get("version", {"query": release_name})
     mark_released = os.environ.get("INPUT_JIRA_MARK_RELEASED", "false").lower() == "true"
+    result = get("version", {"query": release_name})
     if result["total"] == 0:
         return post(
             "version",
-            {"name": release_name, "projectId": get_project_id(), "released": mark_released},
+            {"name": release_name, "projectId": get_project_id()},
         ).json()
+      # immediately mark it released if desired
+      if mark_released:
+        upd = put(f'version/{version["id"]}', {"released": True, "releaseDate": datetime.date.today().isoformat()})
+        upd.raise_for_status()
+        version = upd.json()
+      return version
     elif result["total"] > 1:
         raise Exception("Found multiple releases with the same name.")
     else:
